b56d855466ac86a48f180553b95b596e9da7731f
Ragib Shahriar <ragib.shahriar118@gmail.com>
2026-08-07 20:48:10 +0530

    Use GTM only

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import {
-  GoogleAnalytics,
-  GoogleTagManager,
-} from "@next/third-parties/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 import "./globals.css";
 
@@ -172,9 +169,6 @@ export default function RootLayout({
 
         {children}
 
-        <GoogleAnalytics
-          gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!}
-        />
       </body>
     </html>
   );
