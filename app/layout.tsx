94c8fc61e03f2726cbb30814058bdb494b5af824
ragib118 <ragib.shahriar118@gmail.com>
2026-08-17 17:04:45 +0530

    Improve Google favicon metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,7 +60,7 @@ export const metadata: Metadata = {
   applicationName: "DAO Studios",
   category: "Entertainment",
 
-  manifest: "/manifest.webmanifest",
+  manifest: "/site.webmanifest",
 
   alternates: {
     canonical: "https://www.daostudios.co",
@@ -114,21 +114,28 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
         url: "/favicon.ico",
-      },
-      {
-        url: "/favicon-32x32.png",
-        sizes: "32x32",
-        type: "image/png",
-      },
-      {
-        url: "/favicon-16x16.png",
-        sizes: "16x16",
-        type: "image/png",
+        sizes: "any",
       },
     ],
 
-    apple: "/apple-touch-icon.png",
+    apple: [
+      {
+        url: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
   },
 };
 
